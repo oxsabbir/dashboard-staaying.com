47317b132a0ee446417a4233b951f9f7a3b0d3a7
Sabbir Hossain <sabbir2always@gmail.com>
2026-03-11 12:33:34 +0600

    Complete ImageKit integration for seamless image upload

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,35 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "ik.imagekit.io"],
+          "media-src": ["'self'", "data:", "blob:", "ik.imagekit.io"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "50mb",
+      jsonLimit: "50mb",
+      textLimit: "50mb",
+      formidable: {
+        maxFileSize: 50 * 1024 * 1024,
+      },
+    },
+  },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  imagekit: {
+    enabled: true,
+    config: {
+      publicKey: env("IMAGEKIT_PUBLIC_KEY"),
+      privateKey: env("IMAGEKIT_PRIVATE_KEY"),
+      urlEndpoint: env("IMAGEKIT_URL_ENDPOINT"),
+      enabled: true,
+      useTransformUrls: true,
+      uploadEnabled: true,
+      uploadOptions: {
+        folder: "/strapi-uploads/",
+        tags: ["strapi", "media"],
+      },
+    },
+  },
+});
